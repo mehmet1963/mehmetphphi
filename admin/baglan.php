462374e12fcbccbe843c65fbe8df17b58116ce1a
mehmet1963 <mehmetgider1907@gmail.com>
2022-03-21 19:36:09 +0300

    Dosyalar Oluşturuldu

---
+++ admin/baglan.php
@@ -0,0 +1,5 @@
+<?php
+
+$db = new PDO('mysql:host=localhost;dbname=aribilisim;charset=UTF8','root','');
+
+?>
---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+</div>
+    </div>
+</div>
+</section>
+
+
+
+    <script src="../js/jquery.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+
+</html>
